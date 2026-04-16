14a26260201d7408aee788f2948bf147942fea6d
Andrii Shevchenko <globallogic_andriis@nice.com>
2026-04-16 14:46:24 +0300

    fix(supervisor): align non-windows installService stub signature

--- supervisor/service_stub.go
+++ supervisor/service_stub.go
@@ -4,8 +4,10 @@ package main
 
 import "errors"
 
-func runAsService() error             { return errors.New("not supported on this OS") }
-func installService(_ string) error   { return errors.New("not supported on this OS") }
+func runAsService() error { return errors.New("not supported on this OS") }
+func installService(_, _, _ string) error {
+	return errors.New("not supported on this OS")
+}
 func uninstallService() error         { return errors.New("not supported on this OS") }
 func startService() error             { return errors.New("not supported on this OS") }
 func stopService() error              { return errors.New("not supported on this OS") }
